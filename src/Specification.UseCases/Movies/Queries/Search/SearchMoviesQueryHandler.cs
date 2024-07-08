a6c07c71f44254c74e2427d940036303ed3f141b
Aleksei Mialkin <aleksei@mialkin.ru>
2024-07-09 02:49:03 +0300

    Use FakeTimeProvider

--- src/Specification.UseCases/Movies/Queries/Search/SearchMoviesQueryHandler.cs
+++ src/Specification.UseCases/Movies/Queries/Search/SearchMoviesQueryHandler.cs
@@ -1,5 +1,6 @@
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Time.Testing;
 using Specification.Domain.Entities;
 using Specification.Infrastructure.Interfaces.Database;
 
@@ -12,10 +13,15 @@ public class SearchMoviesQueryHandler(IReadOnlyDatabaseContext readOnlyDatabaseC
         SearchMoviesQuery request,
         CancellationToken cancellationToken)
     {
+        var fakeTimeProvider = new FakeTimeProvider(new DateTimeOffset(new DateTime(2017, 1, 1)));
+
+        // A movie is available on CD if it has been released at least 6 months ago.
+        var halfYearAgo = fakeTimeProvider.GetUtcNow().AddMonths(-6).UtcDateTime;
+
         var movies = await readOnlyDatabaseContext.Movies
             .Where(x => (x.MpaaRating <= MpaaRating.Pg || !request.ForKidsOnly)
                         && x.Rating >= request.MinimumRating
-                        && (x.ReleaseDate <= DateTime.Now.AddMonths(-6) || !request.AvailableOnCd))
+                        && (x.ReleaseDate <= halfYearAgo || !request.AvailableOnCd))
             .Select(x => new SearchMoviesDto(
                 x.Id,
                 x.Name,
